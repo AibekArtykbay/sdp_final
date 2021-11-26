b12078d9fd4d935c2ed760ece8c9a44d22fce1b1
AibekArtykbay <201475@astanait.edu.kz>
2021-11-26 17:27:15 +0600

    Initial commit

---
+++ src/entity/creams/CarrotCream.java
@@ -0,0 +1,12 @@
+package entity.creams;
+
+import strategy.CreamStrategy;
+
+public class CarrotCream implements CreamStrategy {
+
+
+    @Override
+    public void addCream() {
+        System.out.println("Added carrot cream.");
+    }
+}
